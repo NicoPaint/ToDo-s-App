6196925ad13b1805178b41b2c2a2582a80c0f3c0
NicoPaint <felipepinve@gmail.com>
2024-09-28 16:55:20 -0500

    I added data persistence with Local Storage

--- src/Components/App.js
+++ src/Components/App.js
@@ -12,7 +12,7 @@ import Confetti from 'react-confetti';
 //Styles
 import './App.css';
 
-const defaultToDos = [
+/* const defaultToDos = [
   { text: "Ver bettlejuice 2", completed: true},
   { text: "Pagar arriendo", completed: false},
   { text: "Comprar la moto", completed: false},
@@ -25,10 +25,28 @@ const defaultToDos = [
   { text: "Subir los cambios a GitHub", completed: true},
 ]
 
+localStorage.setItem("TODOS_V1", JSON.stringify(defaultToDos));
+localStorage.removeItem("TODOS_V1"); */
+
 function App() {
+
+  let stringifiedToDos = localStorage.getItem("TODOS_V1");  //Se trate la informción que este en LS bajo TODOS_V1 en formato string
+
+  let parsedToDos;  //esta variable guarda la información en LS bajo TODOS_V1 transformada en array nuevamente
+
+  //Se hace la validación para saber si existe algo en TODOS_V1 en LS o no
+  if(!stringifiedToDos){
+    //si no hay, se crea el espacio de TODOS_V1 en LS con un array vacio y parsedToDos se le asigna un array vacio
+    localStorage.setItem("TODOS_V1", JSON.stringify([]));
+    parsedToDos = [];
+  } else{
+    //si si hay, se parsea el string traido de LS y se le asigna a parsedToDos.
+    parsedToDos = JSON.parse(stringifiedToDos);
+  }
+
   //Solo se pueden pasar estados de padres a hijos, por lo que se van a crear la mayoria de ellos en este componente padre.
 
-  const [toDos, setToDos] = React.useState(defaultToDos);  //Se crea el estado para la lista de ToDos de la app que toma como valor inicial el array defaultToDos
+  const [toDos, setToDos] = React.useState(parsedToDos);  //Se crea el estado para la lista de ToDos de la app que toma como valor inicial el array que este guardado bajo TODOS_V1 en local storage
   const [searchValue, setSearchValue] = React.useState('');  //Se crea el estado searchValue para capturar lo que escriben los usuarios y ejecutar tareas con ello
 
   const completedToDos = toDos.filter(todo => todo.completed).length;  //se crea este estado derivado para llevar la cuenta de los ToDOs completados
@@ -43,6 +61,13 @@ function App() {
     return toDoText.includes(searchText); //va a regresar cada uno de los elementos del array que contenga el string que el usuario escriba en la barra. Si es un string vacío va a devolver todo el array original.
   })
 
+  //Esta funcion va hacer la actualización de la lista de ToDos tanto en el LS (persistencia de datos) como en el estado toDos.
+  const saveToDosLS = (newTodos) => {
+    localStorage.setItem("TODOS_V1", JSON.stringify(newTodos));
+
+    setToDos(newTodos);
+  }
+
   //Este estado derivado se usa para hacer la actualización de la lista cada vez que un usuario de click en el icono de completado, ya sea para marcarlo como completado o no. Para ello se le pasa el texto del item para identificarlo
   const toggleToDo = (text) => {
     const newToDos = [...toDos];  //se crea una copia de el estado toDos.
@@ -55,7 +80,7 @@ function App() {
       newToDos[toDoIndex].completed = false;
     }
 
-    setToDos(newToDos);  //Se actualiza el estado de toDos para hacer el render del nuevo listado actualizado
+    saveToDosLS(newToDos);  //Se actualiza el estado de toDos y el LS para hacer el render del nuevo listado actualizado
   }
   
   //Este estado derivado se usa para eliminar un item de la lista cada vez que el usuario da click en el icon de eliminar. Para ello se le pasa el texto del item para identificarlo
@@ -65,7 +90,7 @@ function App() {
 
     newToDos.splice(toDoIndex, 1);  //se usa el metodo splice para eliminar el item seleccionado. Se pasa el indice y un solo elemento para eliminar solo ese unico item.
 
-    setToDos(newToDos);  //Se actualiza el estado de toDos para hacer el render del nuevo listado actualizado
+    saveToDosLS(newToDos);  //Se actualiza el estado de toDos y el LS para hacer el render del nuevo listado actualizado
   }
 
   //Esta funcion contiene la logica para lanzar el confetti cuando se completa todos los items de la lista
@@ -81,7 +106,7 @@ function App() {
 
   return (
     <div className='app'>
-      {completedToDos === totalToDos && shootingConfetti()}  {/* Cuando los completede sean igual a los totales va a disparar el confetti en la app */}
+      {completedToDos === totalToDos && totalToDos !== 0 && shootingConfetti()}  {/* Cuando los completede sean igual a los totales, y existan ToDos, va a disparar el confetti en la app */}
       <TodoCounter 
         total={totalToDos}   /* se pasa como props la cantidad de ToDOs terminados y la cantidad total para mostrarlos en el titulo de la app */
         completed={completedToDos}
