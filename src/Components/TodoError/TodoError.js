48bf8959e6780d730581b221d963e648726defcd
NicoPaint <felipepinve@gmail.com>
2024-10-14 22:10:51 -0500

    I added styles to the error todo message component

--- src/Components/TodoError/TodoError.js
+++ src/Components/TodoError/TodoError.js
@@ -1,8 +1,12 @@
+//Este componente va a manejar el mensaje de error de carga de la base de datos.
+//React
 import React from "react";
+//Styles
+import "./TodoError.css"
 
 function TodoError(){
     return(
-        <p>Hubo un error!!!</p>
+        <p className="error-todos-text">Lo sentimos, hubo un error. Vuelve a cargar la app</p>
     )
 }
 
